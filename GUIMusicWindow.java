b265fd1d7269f6836b8eec1dd9416ce0dc529cdf
kaikato26 <48038305+kaikato26@users.noreply.github.com>
2019-11-16 16:21:10 -0500

    Add files via upload

---
+++ GUIMusicWindow.java
@@ -0,0 +1,75 @@
+package proj5;
+import java.awt.Button;
+import java.awt.Window;
+
+public class GUIMusicWindow {
+    private Window window;
+    private Button repHobby;
+    private Button repRegion;
+    private Button repMajor;
+    private Button quit;
+    private Button prev;
+    private Button next;
+    private Button byArtist;
+    private Button byGenre;
+    private Button byYear;
+    private Button byTitle;
+
+
+    public GUIMusicWindow() {
+        this.window = new Window();
+        window.setTi
+        repHobby = new Button();
+        
+    }
+
+
+    public void clickedRepHobby(Button button) {
+
+    }
+
+
+    public void clickedRepRegion(Button button) {
+
+    }
+
+
+    public void clickedRepMajor(Button button) {
+
+    }
+
+
+    public void clickedQuit(Button button) {
+
+    }
+
+
+    public void clickedPrev(Button button) {
+
+    }
+
+
+    public void clickedNext(Button button) {
+
+    }
+
+
+    public void clickedbyArtist(Button button) {
+
+    }
+
+
+    public void clickedbyGenere(Button button) {
+
+    }
+
+
+    public void clickedbyYear(Button button) {
+
+    }
+
+
+    public void clickedByTitle(Button button) {
+
+    }
+}
